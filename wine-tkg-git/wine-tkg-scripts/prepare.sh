0a25fec7879d4149e08327c2ac6b926a2504aed8
Tk-Glitch <ti3nou@gmail.com>
2020-04-04 13:40:51 +0200

    Update leagueoflolfix for 3b16f35 and make it explicitly require staging. That was already the case before, but not explicitely.

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -943,9 +943,11 @@ _prepare() {
 	fi
 
 	# Fix for LoL 9.20+ crashing - https://bugs.winehq.org/show_bug.cgi?id=47198
-	if [ "$_lol920_fix" == "true" ]; then
-	  if git merge-base --is-ancestor b8f0e32b9f00f63abee6ca31e190ff794c053b67 HEAD; then
+	if [ "$_lol920_fix" == "true" ] && [ "$_use_staging" == "true" ]; then
+	  if git merge-base --is-ancestor 3b16f35413f3a6641df42b782ead294f343e7d5e HEAD; then
 	    _patchname='leagueoflolfix.patch' && _patchmsg="Applied LoL 9.20+ fix - Requires vdso32 disabled (echo 0 > /proc/sys/abi/vsyscall32)" && nonuser_patcher
+	  elif git merge-base --is-ancestor b8f0e32b9f00f63abee6ca31e190ff794c053b67 HEAD; then
+	    _patchname='leagueoflolfix-3b16f35.patch' && _patchmsg="Applied LoL 9.20+ fix - Requires vdso32 disabled (echo 0 > /proc/sys/abi/vsyscall32)" && nonuser_patcher
 	  elif git merge-base --is-ancestor 39138478fdd93cc0dfc1e83b85784bc468e8d237 HEAD; then
 	    _patchname='leagueoflolfix-b8f0e32.patch' && _patchmsg="Applied LoL 9.20+ fix - Requires vdso32 disabled (echo 0 > /proc/sys/abi/vsyscall32)" && nonuser_patcher
 	  elif git merge-base --is-ancestor 944c4e8f760460ca6a260573d87c454052caad2c HEAD; then
