793dc0b479d23753a3976f7b554ea46ef3805f11
Tk-Glitch <ti3nou@gmail.com>
2020-06-06 18:17:56 +0200

    Allow enforcing _dxvk_minimald3d10="false"

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -47,7 +47,7 @@ _exit_cleanup() {
     echo "_NUKR='${_NUKR}'" >> "$_proton_tkg_path"/proton_tkg_token
     echo "_winesrcdir='${_winesrcdir}'" >> "$_proton_tkg_path"/proton_tkg_token
     echo "_standard_dlopen='${_standard_dlopen}'" >> "$_proton_tkg_path"/proton_tkg_token
-    if $(cd "${srcdir}"/"${_winesrcdir}" && git merge-base --is-ancestor 1e478b804f72a9b5122fc6adafac5479b816885e HEAD); then
+    if ( cd "${srcdir}"/"${_winesrcdir}" && git merge-base --is-ancestor 1e478b804f72a9b5122fc6adafac5479b816885e HEAD ) && [ "$_dxvk_minimald3d10" != "false" ]; then
       echo "_dxvk_minimald3d10='true'" >> "$_proton_tkg_path"/proton_tkg_token
     fi
   fi
