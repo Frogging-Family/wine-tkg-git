Frogging-Family/wine-tkg-git

commit ef6b497379b4597c7fc0f9340a3cb1f289098f63

Don't disable winevulkan_vkGetPhysicalDeviceSurfaceCapabilitiesKHR on staging 23ae4e6+ (#102)
The patch was removed

wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -638,7 +638,7 @@ _prepare() {
 	if [ "$_update_winevulkan" = "true" ] && ! git merge-base --is-ancestor 3e4189e3ada939ff3873c6d76b17fb4b858330a8 HEAD && git merge-base --is-ancestor eb39d3dbcac7a8d9c17211ab358cda4b7e07708a HEAD; then
 	  _patchname='winevulkan-1.1.103.patch' && _patchmsg="Applied winevulkan 1.1.103 patch" && nonuser_patcher
 	fi
-	if ( [ "$_update_winevulkan" = "true" ] || [ "$_proton_fs_hack" = "true" ] ) && ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor df2fd22e4de96b28eb0ced5e8aa9bf4c421b5ed8 HEAD ); then
+	if ( [ "$_update_winevulkan" = "true" ] || [ "$_proton_fs_hack" = "true" ] ) && ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor df2fd22e4de96b28eb0ced5e8aa9bf4c421b5ed8 HEAD ) && ( [ "$_hotfixansw" != "N" ] && [ "$_hotfixansw" != "n" ] ) || ( cd "${srcdir}"/"${_stgsrcdir}" && ! git merge-base --is-ancestor 23ae4e6c7ac3b4c463ea79944dac62affe6173bf HEAD ); then
 	  _staging_args+=(-W winevulkan-vkGetPhysicalDeviceSurfaceCapabilitiesKHR)
 	fi
 

commit 3ce94913f8bf5d3a2fb3adc3d06e9661896eb655

Fix broken rule introduced by https://github.com/Frogging-Family/wine-tkg-git/pull/102
This was preventing legacy compilation

wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -640,7 +640,7 @@ _prepare() {
 	if [ "$_update_winevulkan" = "true" ] && ! git merge-base --is-ancestor 3e4189e3ada939ff3873c6d76b17fb4b858330a8 HEAD && git merge-base --is-ancestor eb39d3dbcac7a8d9c17211ab358cda4b7e07708a HEAD; then
 	  _patchname='winevulkan-1.1.103.patch' && _patchmsg="Applied winevulkan 1.1.103 patch" && nonuser_patcher
 	fi
-	if ( [ "$_update_winevulkan" = "true" ] || [ "$_proton_fs_hack" = "true" ] ) && ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor df2fd22e4de96b28eb0ced5e8aa9bf4c421b5ed8 HEAD ) && ( [ "$_hotfixansw" != "N" ] && [ "$_hotfixansw" != "n" ] ) || ( cd "${srcdir}"/"${_stgsrcdir}" && ! git merge-base --is-ancestor 23ae4e6c7ac3b4c463ea79944dac62affe6173bf HEAD ); then
+	if ( [ "$_update_winevulkan" = "true" ] || [ "$_proton_fs_hack" = "true" ] ) && ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor df2fd22e4de96b28eb0ced5e8aa9bf4c421b5ed8 HEAD && ! git merge-base --is-ancestor 23ae4e6c7ac3b4c463ea79944dac62affe6173bf HEAD ); then
 	  _staging_args+=(-W winevulkan-vkGetPhysicalDeviceSurfaceCapabilitiesKHR)
 	fi
 