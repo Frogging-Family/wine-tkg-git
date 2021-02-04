553dee13bbfde768cb545d76152d750e7e64e5af
Tk-Glitch <ti3nou@gmail.com>
2021-02-04 12:58:42 +0100

    Update proton-tkg meta patchsets for c6d85fc and move previous versions to legacy

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -1683,7 +1683,7 @@ EOM
 	echo -e "" >> "$_where"/last_build_config.log
 
 	if [ "$_EXTERNAL_INSTALL" = "true" ] && [ "$_EXTERNAL_INSTALL_TYPE" = "proton" ] && [ "$_unfrog" != "true" ] && ! git merge-base --is-ancestor 74dc0c5df9c3094352caedda8ebe14ed2dfd615e HEAD || ([ "$_protonify" = "true" ] && git merge-base --is-ancestor 74dc0c5df9c3094352caedda8ebe14ed2dfd615e HEAD); then
-	  if ( cd "${srcdir}"/"${_winesrcdir}" && git merge-base --is-ancestor d4bb0c4639aea2e8df80e577bd20b7f51c033e33 HEAD ); then
+	  if ( cd "${srcdir}"/"${_winesrcdir}" && git merge-base --is-ancestor c6d85fc2178094840a5ea2cd4823c7b68e48e473 HEAD ); then
 	    if [ "$_use_staging" = "true" ]; then
 	      if ! git merge-base --is-ancestor dedd5ccc88547529ffb1101045602aed59fa0170 HEAD; then
 	        _patchname='proton-tkg-staging-rpc.patch' && _patchmsg="Using Steam-specific Proton-tkg patches (staging) 1/3" && nonuser_patcher
@@ -1745,7 +1745,11 @@ EOM
 	      fi
 	    fi
 	  else
-	    if git merge-base --is-ancestor 6f158754435f403864052e595ab627dadac2666f HEAD; then
+	    if git merge-base --is-ancestor d4bb0c4639aea2e8df80e577bd20b7f51c033e33 HEAD; then
+	      _lastcommit="c6d85fc"
+	      _rpc="1"
+	      _stmbits="1"
+	    elif git merge-base --is-ancestor 6f158754435f403864052e595ab627dadac2666f HEAD; then
 	      _lastcommit="d4bb0c4"
 	      _rpc="1"
 	      _stmbits="1"
