6e3413af6af35005a3cdc3856d71d45c727983ab
Tk-Glitch <ti3nou@gmail.com>
2021-06-18 19:07:24 +0200

    Tighten pkgver check

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 _exit_cleanup() {
-  sed -i "s/pkgver=$pkgver/pkgver=0/g" "$_where"/PKGBUILD
+  sed -i "s/pkgver=$pkgver.*/pkgver=0/g" "$_where"/PKGBUILD
 
   # Proton-tkg specifics to send to token
   if [ -e "$_where"/BIG_UGLY_FROGMINER ] && [ "$_EXTERNAL_INSTALL" = "proton" ] && [ -n "$_proton_tkg_path" ]; then
