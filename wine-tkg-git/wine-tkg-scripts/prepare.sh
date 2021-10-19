Frogging-Family/wine-tkg-git

commit 566a8b12bf266de64657d9bd6950ddf3c557bd19

Update valve_proton_fullscreen_hack-staging patchset for 6dcaff4 and move previous version to legacy

wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -1641,10 +1641,12 @@ EOM
 	    _patchname='FS_bypass_compositor.patch' && _patchmsg="Applied Fullscreen compositor bypass patch" && nonuser_patcher
 	  fi
 	  if [ "$_use_staging" = "true" ]; then
-	    if git merge-base --is-ancestor a7ec245f844762cce6fb789fe3ffb1bf42d44249 HEAD; then
+	    if git merge-base --is-ancestor 6dcaff421f87a93efe18b2efe0ec64d94ed1d483 HEAD; then
 	      _patchname='valve_proton_fullscreen_hack-staging.patch' && _patchmsg="Applied Proton fullscreen hack patch (staging)" && nonuser_patcher
 	    else
-	      if git merge-base --is-ancestor f46c4a3920ce8e96b37b606c207add7f596f1950 HEAD; then
+	      if git merge-base --is-ancestor a7ec245f844762cce6fb789fe3ffb1bf42d44249 HEAD; then
+	        _lastcommit="6dcaff4"
+	      elif git merge-base --is-ancestor f46c4a3920ce8e96b37b606c207add7f596f1950 HEAD; then
 	        _lastcommit="a7ec245"
 	      elif git merge-base --is-ancestor 8285f616030f27877922ff414530d4f909306ace HEAD; then
 	        _lastcommit="f46c4a3"
@@ -2856,7 +2858,7 @@ _polish() {
 
 	echo -e "\nRunning make_vulkan" >> "$_where"/prepare.log && dlls/winevulkan/make_vulkan >> "$_where"/prepare.log 2>&1
 	tools/make_requests
-	autoreconf -f
+	autoreconf -fiv
 
 	# The versioning string has moved with 1dd3051cca5cafe90ce44460731df61abb680b3b
 	# Since this is reverted by the hotfixer path, only use the new path on 0c249e6+ (deprecation of the hotfixer path)

wine-tkg-git/PKGBUILD
@@ -419,7 +419,7 @@ md5sums=('SKIP'
          '5ca4ee142eafe2c8873d59b2feb51a7d'
          '684d1c60aa38a659b1dd792b1d7cd8f5'
          '84151397310fcbbe70d44f55eb7100a5'
-         '3d5a4b4d5d2114b0f00ce47c7151c933'
+         '0ef5a2f17eb4f44f213129824fae7c81'
          '4df9cd990a78606d3cd0dc0673bb141f'
          'a784c182c65c20cbb29b6473abae171d'
          'f2db9103d6527377746540a9f8860c2f'

wine-tkg-git/wine-tkg-patches/proton/legacy/valve_proton_fullscreen_hack-staging-6dcaff4.patch
@@ -7032,7 +7032,26 @@ diff --git a/dlls/winex11.drv/vulkan.c b/dlls/winex11.drv/vulkan.c
 index 5563af5fa41..7230b778cc5 100644
 --- a/dlls/winex11.drv/vulkan.c
 +++ b/dlls/winex11.drv/vulkan.c
-@@ -215,16 +220,30 @@ static void wine_vk_surface_release(struct wine_vk_surface *surface)
+@@ -60,6 +60,7 @@ static XContext vulkan_swapchain_context;
+ 
+ struct wine_vk_surface
+ {
++    struct list entry;
+     LONG ref;
+     Window window;
+     VkSurfaceKHR surface; /* native surface */
+@@ -208,6 +209,10 @@ static void wine_vk_surface_release(struct wine_vk_surface *surface)
+     if (InterlockedDecrement(&surface->ref))
+         return;
+ 
++    EnterCriticalSection(&context_section);
++    list_remove(&surface->entry);
++    LeaveCriticalSection(&context_section);
++
+     if (surface->window)
+         XDestroyWindow(gdi_display, surface->window);
+ 
+@@ -215,14 +220,28 @@ static void wine_vk_surface_release(struct wine_vk_surface *surface)
  }
  
  void wine_vk_surface_destroy(HWND hwnd)
@@ -7056,8 +7075,6 @@ index 5563af5fa41..7230b778cc5 100644
 -    if (!XFindContext(gdi_display, (XID)hwnd, vulkan_hwnd_context, (char **)&surface))
 +    if (!XFindContext(gdi_display, (XID)hwnd, vulkan_hwnd_context, (char **)&surface_list))
      {
-         surface->hwnd_thread_id = 0;
-         surface->hwnd = NULL;
 -        wine_vk_surface_release(surface);
 +        LIST_FOR_EACH_ENTRY(surface, surface_list, struct wine_vk_surface, entry)
 +            if (surface->window != active) XConfigureWindow(gdi_display, surface->window, mask, changes);
@@ -7066,29 +7083,40 @@ index 5563af5fa41..7230b778cc5 100644
      LeaveCriticalSection(&context_section);
  }
  
-@@ -298,6 +317,7 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
+@@ -298,7 +317,8 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
+ {
      VkResult res;
      VkXlibSurfaceCreateInfoKHR create_info_host;
-     struct wine_vk_surface *x11_surface;
+-    struct wine_vk_surface *x11_surface, *prev;
++    struct wine_vk_surface *x11_surface;
 +    struct list *surface_list;
  
      TRACE("%p %p %p %p\n", instance, create_info, allocator, surface);
  
 @@ -304,6 +322,7 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
-     {
-         x11_surface->window = create_dummy_client_window();
-     }
+     x11_surface->hwnd = create_info->hwnd;
+     x11_surface->window = x11_surface->hwnd ? create_client_window(create_info->hwnd, &default_visual)
+                                             : create_dummy_client_window();
 +    list_init(&x11_surface->entry);
  
      if (!x11_surface->window)
      {
-@@ -329,7 +348,16 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
+@@ -329,12 +348,23 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
      if (x11_surface->hwnd)
      {
-         wine_vk_surface_destroy( x11_surface->hwnd );
+         EnterCriticalSection(&context_section);
+-        if (!XFindContext(gdi_display, (XID)create_info->hwnd, vulkan_hwnd_context, (char **)&prev))
++        if (XFindContext(gdi_display, (XID)create_info->hwnd, vulkan_hwnd_context, (char **)&surface_list) &&
++                (surface_list = heap_alloc_zero(sizeof(*surface_list))))
+         {
+-            wine_vk_surface_release(prev);
++            list_init(surface_list);
++            XSaveContext(gdi_display, (XID)create_info->hwnd, vulkan_hwnd_context, (char *)surface_list);
+         }
 -        XSaveContext(gdi_display, (XID)create_info->hwnd, vulkan_hwnd_context, (char *)wine_vk_surface_grab(x11_surface));
 +        if (!XFindContext(gdi_display, (XID)create_info->hwnd, vulkan_hwnd_context, (char **)&surface_list))
 +            list_add_tail(surface_list, &x11_surface->entry);
+         LeaveCriticalSection(&context_section);
 +
 +        if (!surface_list)
 +        {
@@ -7098,8 +7126,8 @@ index 5563af5fa41..7230b778cc5 100644
 +        }
 +
      }
-     list_add_tail(&surface_list, &x11_surface->entry);
-     LeaveCriticalSection(&context_section);
+ 
+     *surface = (uintptr_t)x11_surface;
 diff --git a/dlls/winex11.drv/window.c b/dlls/winex11.drv/window.c
 index 559d9e7880a..eb508c05b5d 100644
 --- a/dlls/winex11.drv/window.c
@@ -7121,9 +7149,9 @@ index b41b9d6ad08..faebd7ffcd5 100644
  extern void destroy_gl_drawable( HWND hwnd ) DECLSPEC_HIDDEN;
  extern void wine_vk_surface_destroy( HWND hwnd ) DECLSPEC_HIDDEN;
 +extern void resize_vk_surfaces( HWND hwnd, Window active, int mask, XWindowChanges *changes ) DECLSPEC_HIDDEN;
- extern void vulkan_thread_detach(void) DECLSPEC_HIDDEN;
  
  extern void wait_for_withdrawn_state( HWND hwnd, BOOL set ) DECLSPEC_HIDDEN;
+ extern Window init_clip_window(void) DECLSPEC_HIDDEN;
 From 92c5b6ea2e8e79253e5c336f871399c14d1c8cad Mon Sep 17 00:00:00 2001
 From: =?UTF-8?q?R=C3=A9mi=20Bernon?= <rbernon@codeweavers.com>
 Date: Fri, 12 Mar 2021 18:07:04 +0100
@@ -7147,9 +7175,9 @@ index 7230b778cc5..b4d674f78e8 100644
 --- a/dlls/winex11.drv/vulkan.c
 +++ b/dlls/winex11.drv/vulkan.c
 @@ -64,6 +64,7 @@ struct wine_vk_surface
+     VkPresentModeKHR present_mode;
      BOOL offscreen; /* drawable is offscreen */
      HWND hwnd;
-     DWORD hwnd_thread_id;
 +    HDC dc;
  };
  
@@ -7181,9 +7209,9 @@ index 7230b778cc5..b4d674f78e8 100644
      heap_free(surface);
  }
 @@ -322,6 +339,7 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
-     {
-         x11_surface->window = create_dummy_client_window();
-     }
+     x11_surface->hwnd = create_info->hwnd;
+     x11_surface->window = x11_surface->hwnd ? create_client_window(create_info->hwnd, &default_visual)
+                                             : create_dummy_client_window();
 +    x11_surface->dc = GetDC(create_info->hwnd);
      list_init(&x11_surface->entry);
  
@@ -7734,9 +7762,9 @@ index 2af04cc1d89..f2a2f736000 100644
 --- a/dlls/winex11.drv/x11drv.h
 +++ b/dlls/winex11.drv/x11drv.h
 @@ -610,6 +610,7 @@ extern void set_gl_drawable_parent( HWND hwnd, HWND parent ) DECLSPEC_HIDDEN;
+ extern void destroy_gl_drawable( HWND hwnd ) DECLSPEC_HIDDEN;
  extern void wine_vk_surface_destroy( HWND hwnd ) DECLSPEC_HIDDEN;
  extern void resize_vk_surfaces( HWND hwnd, Window active, int mask, XWindowChanges *changes ) DECLSPEC_HIDDEN;
- extern void vulkan_thread_detach(void) DECLSPEC_HIDDEN;
 +extern void sync_vk_surface( HWND hwnd, BOOL known_child ) DECLSPEC_HIDDEN;
  
  extern void wait_for_withdrawn_state( HWND hwnd, BOOL set ) DECLSPEC_HIDDEN;

commit a7b5e942d7fffb001e44f592081f072c496d469f

Hotfixer: Add a selective revert patch for staging 164b361 and revert 6dcaff42 when using childwindow or fshack
Our rebase in https://github.com/Frogging-Family/wine-tkg-git/commit/566a8b12bf266de64657d9bd6950ddf3c557bd19 broke some apps using vk childwindow. This fixes the issue until we get a working rebase.

wine-tkg-git/wine-tkg-patches/hotfixes/childwindow/staging-6dcaff42.mystagingpatch
@@ -1,0 +1,45 @@
+From 37c9b11188387ec5eebbf88647c51562e6230303 Mon Sep 17 00:00:00 2001
+From: Tk-Glitch <ti3nou@gmail.com>
+Date: Tue, 19 Oct 2021 14:36:36 +0200
+Subject: Selective revert of staging 164b361be646a1e23fad1892893821de3805c5c6
+
+diff --git a/patches/user32-rawinput-mouse/0008-winex11.drv-Listen-to-RawMotion-and-RawButton-events.patch b/patches/user32-rawinput-mouse/0008-winex11.drv-Listen-to-RawMotion-and-RawButton-events.patch
+index c313a16c0..bf18664d1 100644
+--- a/patches/user32-rawinput-mouse/0008-winex11.drv-Listen-to-RawMotion-and-RawButton-events.patch
++++ b/patches/user32-rawinput-mouse/0008-winex11.drv-Listen-to-RawMotion-and-RawButton-events.patch
+@@ -245,9 +245,9 @@ index 7a89a010395..2fd8519971d 100644
+ --- a/dlls/winex11.drv/x11drv_main.c
+ +++ b/dlls/winex11.drv/x11drv_main.c
+ @@ -647,6 +647,8 @@ void CDECL X11DRV_ThreadDetach(void)
++
+      if (data)
+      {
+-         vulkan_thread_detach();
+ +        if (GetWindowThreadProcessId( GetDesktopWindow(), NULL ) == GetCurrentThreadId())
+ +            x11drv_xinput_disable( data->display, DefaultRootWindow( data->display ), PointerMotionMask );
+          if (data->xim) XCloseIM( data->xim );
+
+diff --git a/patches/winex11-Vulkan_support/0001-winex11-Specify-a-default-vulkan-driver-if-one-not-f.patch b/patches/winex11-Vulkan_support/0001-winex11-Specify-a-default-vulkan-driver-if-one-not-f.patch
+index fccbec73..9f5ba3e9 100644
+--- a/patches/winex11-Vulkan_support/0001-winex11-Specify-a-default-vulkan-driver-if-one-not-f.patch
++++ b/patches/winex11-Vulkan_support/0001-winex11-Specify-a-default-vulkan-driver-if-one-not-f.patch
+@@ -49,7 +49,7 @@ index 4f6624b3db8..6e343f47f34 100644
+          return TRUE;
+      }
+  
+-@@ -735,20 +745,3 @@ const struct vulkan_funcs *get_vulkan_driver(UINT version)
++@@ -735,16 +745,3 @@ const struct vulkan_funcs *get_vulkan_driver(UINT version)
+      return NULL;
+  }
+  
+@@ -65,10 +65,6 @@ index 4f6624b3db8..6e343f47f34 100644
+ -{
+ -}
+ -
+--void vulkan_thread_detach(void)
+--{
+--}
+--
+ -#endif /* SONAME_LIBVULKAN */
+ -- 
+ 2.33.0

wine-tkg-git/wine-tkg-patches/hotfixes/hotfixer
@@ -525,6 +525,12 @@ else
   fi
 fi
 
+# 6dcaff4 breaks childwindow patches (from fshack or standalone), so let's revert the changes for now
+if [ "$_proton_fs_hack" = "true" ] || [ "$_childwindow_fix" = "true" ] && ( cd "${srcdir}"/"${_winesrcdir}" && git merge-base --is-ancestor 6dcaff421f87a93efe18b2efe0ec64d94ed1d483 HEAD ); then
+  _hotfix_mainlinereverts+=(6dcaff421f87a93efe18b2efe0ec64d94ed1d483)
+  _hotfixes+=("$_where"/wine-tkg-patches/hotfixes/childwindow/staging-6dcaff42)
+fi
+
 # Revert d4259ac8e9326a8173d6f83f0d525c97b90a1da0 which breaks compiling the steam helper
 if [ "$_EXTERNAL_INSTALL" = "proton" ]; then
   if ( cd "${srcdir}"/"${_winesrcdir}" && git merge-base --is-ancestor 9ae627316d98b3a0399bd6b5c2257bd47d596bc6 HEAD ); then

wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -2857,7 +2857,7 @@ _polish() {
 
 	echo -e "\nRunning make_vulkan" >> "$_where"/prepare.log && dlls/winevulkan/make_vulkan >> "$_where"/prepare.log 2>&1
 	tools/make_requests
-	autoreconf -f
+	autoreconf -fiv
 
 	# The versioning string has moved with 1dd3051cca5cafe90ce44460731df61abb680b3b
 	# Since this is reverted by the hotfixer path, only use the new path on 0c249e6+ (deprecation of the hotfixer path)