54669e1ff5bbe15fab62e4bafc4478197ad42c7f
Tk-Glitch <ti3nou@gmail.com>
2022-07-08 16:05:44 +0200

    makepkg: Store the _bleeding_tag in "$_where"/temp, else makepkg looses the value
    Fixes ee518e7d97a88d4ececd170db0f408eb98a4861e

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -461,6 +461,7 @@ user_patcher() {
 
 _describe_wine() {
   if [ "$_LOCAL_PRESET" = "valve-exp-bleeding" ]; then
+    source "$_where"/temp
     # On experimental bleeding edge, we want to keep only the first 7 out of 13 bits
     echo "$_bleeding_tag" | sed 's/\([^-]*-g\)/r\1/;s/-/./g;s/^v//;s/\.rc/rc/;s/^wine\.//;s/\.wine//' | cut -d'.' -f1-7
   else
