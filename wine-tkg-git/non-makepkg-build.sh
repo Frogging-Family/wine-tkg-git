32e21f6c9641f1fea7a083656f8b146feef1076b
loopyd <nightwintertooth@gmail.com>
2024-03-15 20:06:42 -0700

    Add command-line options to non-makepkg-build.sh and cleanup the script a bit.

--- wine-tkg-git/non-makepkg-build.sh
+++ wine-tkg-git/non-makepkg-build.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 
-# Created by: Tk-Glitch <ti3nou at gmail dot com>
-
+# ===========================================================================================================================================================================
+# non-makepkg-build.sh - A non-makepkg build script for wine-tkg-git
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#     Created by: Tk-Glitch <ti3nou at gmail dot com>
+#     Updated by: loopyd <nightwintertooth at gmail dot com>
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
 # This script replaces the wine-tkg PKGBUILD's function for use outside of makepkg or on non-pacman distros
-
-## You can check for missing dependencies by running this script with either `--deps64` argument for 64-bit dependencies or `--deps32` argument for 32-bit dependencies :
+#
+# For command-line help, run this script with the `-h|--help` argument :
+#
+# ./non-makepkg-build.sh -h
+#
+# You can check for missing dependencies by running this script with either `-d|--deps64` argument for 64-bit dependencies or `e|--deps32` argument for 32-bit dependencies :
+#
 # ./non-makepkg-build.sh --deps64
 # ./non-makepkg-build.sh --deps32
-
-## On a stock Ubuntu 19.04 install, you'll need the following deps as bare minimum to build default wine-tkg (without Faudio/winevulkan):
-# pkg-config (or pkgconf) bison flex schedtool libfreetype6-dev xserver-xorg-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+#
+# If you want to specify a custom config file, you can use the `--config` argument :
+#
+# ./non-makepkg-build.sh --config /path/to/your/config/file
+#
+# On a stock Ubuntu 19.04 install, you'll need the following deps as bare minimum to build default wine-tkg (without Faudio/winevulkan):
+#
+#   pkg-config (or pkgconf) bison flex schedtool libfreetype6-dev xserver-xorg-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+#
 # For 32-bit support : gcc-multilib g++-multilib libfreetype6-dev:i386 xserver-xorg-dev:i386 libgstreamer1.0-dev:i386 libgstreamer-plugins-base1.0-dev:i386
-
-## For proton-tkg, the 32-bit dependencies above are required as well as the following additions:
-# curl fontforge fonttools libsdl2-dev python3-tk
-# !!! _sdl_joy_support="true" (default setting) would require libsdl2-dev:i386 but due to a conflict between libsdl2-dev:i386 and libmirclientcpp-dev (at least on 19.04) you're kinda frogged and should set _sdl_joy_support to "false" to build successfully. You'll lose SDL2 support on 32-bit apps !!!
-
-## You're on your own to resolve additional dependencies you might want to build with, such as Faudio.
+#
+# For proton-tkg, the 32-bit dependencies above are required as well as the following additions:
+#
+#   curl fontforge fonttools libsdl2-dev python3-tk
+#
+# !!! _sdl_joy_support="true" (default setting) would require libsdl2-dev:i386 but due to a conflict between libsdl2-dev:i386 and libmirclientcpp-dev 
+#     (at least on 19.04) you're kinda frogged and should set _sdl_joy_support to "false" to build successfully. You'll lose SDL2 support on 32-bit apps !!!
+#
+# You're on your own to resolve additional dependencies you might want to build with, such as Faudio.
+#
+# ===========================================================================================================================================================================
 
 pkgname=wine-tkg
 _build_in_tmpfs="true"
 _esyncsrcdir='esync'
-_where="$PWD" # track basedir as different Arch based distros are moving srcdir around
+_where="$PWD" # Track the base directory as different Arch-based distros are moving srcdir around
 
-# set srcdir, Arch style
+# Set the srcdir, Arch Linux style
 if [ "$_build_in_tmpfs" = "true" ]; then
   rm -rf "$_where"/src
   mkdir -p /tmp/wine-tkg/src
@@ -33,15 +54,31 @@ else
 fi
 srcdir="$_where"/src
 
-# use msg2, error and pkgver funcs for compat
+# Source common functions
+source "$_where"/wine-tkg-scripts/prepare.sh
+source "$_where"/wine-tkg-scripts/build.sh
+
+_DEPSHELPER=${_DEPSHELPER:-0}
+ACTION="build"
+
+# Print a basic message (included for compatibility with makepkg)
+msg() {
+  echo -e " \033[1;34m->\033[1;0m $1" >&2
+}
+
+# Print a message with a prefix (included for compatibility with makepkg)
 msg2() {
-  echo -e " \033[1;34m->\033[1;0m \033[1;1m$1\033[1;0m" >&2
+  echo -e " \033[1;34m=>\033[1;0m \033[1;1m$1\033[1;0m" >&2
 }
-error() {
-  echo -e " \033[1;31m==> ERROR: $1\033[1;0m" >&2
-}
+
+# Print a warning message (included for compatibility with makepkg)
 warning() {
   echo -e " \033[1;33m==> WARNING: $1\033[1;0m" >&2
+}
+
+# Print an error message (included for compatibility with makepkg)
+error() {
+  echo -e " \033[1;31m===> ERROR: $1\033[1;0m" >&2
 }
 
 pkgver() {
@@ -51,64 +88,11 @@ pkgver() {
     else
       cd "${srcdir}/${_winesrcdir}"
     fi
-
-    # retrieve current wine version - if staging is enabled, staging version will be used instead
-    _describe_wine
+    _describe_wine      # Retrieve current Wine version - if staging is enabled, staging version will be used instead.
   fi
 }
 
-# The dependency "helper" (running configure) doesn't have to go through the initial prompt, so skip it
-if [ "$1" = "--deps64" ] || [ "$1" = "--deps32" ]; then
-  _DEPSHELPER=1
-fi
-
-# load functions
-source "$_where"/wine-tkg-scripts/prepare.sh
-source "$_where"/wine-tkg-scripts/build.sh
-
-msg2 "Non-makepkg build script will be used.\n"
-
-# init step
-_init
-
-# dependencies
-if [[ "$_nomakepkg_dependency_autoresolver" == "true" ]] && [ "$_DEPSHELPER" != "1" ]; then
-  source "$_where"/wine-tkg-scripts/deps
-  if [[ "${_ci_build}" != "true" ]]; then
-    warning "PLEASE MAKE SURE TO READ https://github.com/Frogging-Family/wine-tkg-git/issues/773 BEFORE ATTEMPTING TO USE \"debuntu\" dependency resolution"
-    read -rp "Either press enter to continue, or ctrl+c to leave."
-  fi
-  if [[ "$_NOLIB64" != "true" ]]; then
-    install_deps "64" "${_ci_build}" || {
-      error "64-bit dependencies installation failed. Please check the error message and install the missing dependencies manually."
-      exit 1
-    }
-  fi
-  if [[ "$_NOLIB32" != "true" ]]; then
-    install_deps "32" "${_ci_build}" || {
-      error "32-bit dependencies installation failed. Please check the error message and install the missing dependencies manually."
-      exit 1
-    }
-  fi
-fi
-
-# this script makes external builds already and we don't want the specific pacman-related stuff to interfere, so enforce _EXTERNAL_INSTALL="false" when not building proton-tkg
-if [ "$_EXTERNAL_INSTALL" = "true" ]; then
-  _EXTERNAL_INSTALL="false"
-fi
-
-# disable faudio check so we don't fail to build even if faudio libs are missing
-_faudio_ignorecheck="true"
-
-if [ -z "$_localbuild" ]; then
-  _pkgnaming
-fi
-
-# remove the faudio pkgname tag as we can't be sure it'll get used even if enabled
-pkgname="${pkgname/-faudio-git/}"
-# init step end
-
-_nomakepkgsrcinit() {
+_src_init() {
   # Wine source
   if [ "$_github_mirrorsrc" = "true" ]; then
     _winesrcdir="wine-mirror-git"
@@ -128,8 +112,12 @@ _nomakepkgsrcinit() {
   fi
 
   if [ "$_NUKR" != "debug" ]; then
-    $(find "$_where"/wine-tkg-patches -type f '(' -iname '*patch' -or -iname '*.conf' ')' -not -path "*hotfixes*" -exec cp -n {} "$_where" \;) # copy patches inside the PKGBUILD's dir to preserve makepkg sourcing and md5sum checking
-    $(find "$_where"/wine-tkg-userpatches -type f -name "*.my*" -exec cp -n {} "$_where" \;)                                                   # copy userpatches inside the PKGBUILD's dir
+    # Performing the following:
+    #
+    #   - copy patches inside the PKGBUILD's dir to preserve makepkg sourcing and md5sum checking
+    #   - copy userpatches inside the PKGBUILD's dir
+    find "$_where"/wine-tkg-patches -type f '(' -iname '*patch' -or -iname '*.conf' ')' -not -path "*hotfixes*" -exec cp -n {} "$_where" \;
+    find "$_where"/wine-tkg-userpatches -type f -name "*.my*" -exec cp -n {} "$_where" \;
 
     ## Handle git repos similarly to makepkg to preserve repositories when building both with and without makepkg on Arch
     # Wine source
@@ -147,7 +135,7 @@ _nomakepkgsrcinit() {
       echo "${_stgsrcdir} is not a clone of $(git config --get remote.origin.url) (\"${_stgsrctarget}\"). Let's nuke stuff to get back on track, hopefully." >>"$_where"/prepare.log
       rm -rf "$_where/${_stgsrcdir}" && rm -rf "${srcdir}/${_stgsrcdir}"
       warning "Your ${_stgsrcdir} clone was deleted due to remote mismatch (\"${_stgsrctarget}\" differs from \"$(git config --get remote.origin.url)\"). Let's try again with a fresh clone."
-      _nomakepkgsrcinit
+      _src_init
     fi
     git fetch --all -p
     rm -rf "${srcdir}/${_stgsrcdir}" && git clone "$_where"/"${_stgsrcdir}" "${srcdir}/${_stgsrcdir}"
@@ -163,7 +151,7 @@ _nomakepkgsrcinit() {
       echo "${_winesrcdir} is not a clone of $(git config --get remote.origin.url) (\"${_winesrctarget}\"). Let's nuke stuff to get back on track, hopefully." >>"$_where"/prepare.log
       rm -rf "$_where/${_winesrcdir}" && rm -rf "${srcdir}/${_winesrcdir}"
       warning "Your ${_winesrcdir} clone was deleted due to remote mismatch (\"${_winesrctarget}\" differs from \"$(git config --get remote.origin.url)\"). Let's try again with a fresh clone."
-      _nomakepkgsrcinit
+      _src_init
     fi
     git fetch --all -p
     rm -rf "${srcdir}/${_winesrcdir}" && git clone "$_where"/"${_winesrcdir}" "${srcdir}/${_winesrcdir}"
@@ -191,6 +179,47 @@ _nomakepkgsrcinit() {
   fi
 }
 
+_script_init() {
+  msg2 "Non-makepkg build script will be used.\n"
+  _init
+
+  # dependencies
+  if [[ "$_nomakepkg_dependency_autoresolver" == "true" ]] && [ "$_DEPSHELPER" != "1" ]; then
+    source "$_where"/wine-tkg-scripts/deps
+    if [[ "${_ci_build}" != "true" ]]; then
+      warning "PLEASE MAKE SURE TO READ https://github.com/Frogging-Family/wine-tkg-git/issues/773 BEFORE ATTEMPTING TO USE \"debuntu\" dependency resolution"
+      read -rp "Either press enter to continue, or ctrl+c to leave."
+    fi
+    if [[ "$_NOLIB64" != "true" ]]; then
+      install_deps "64" "${_ci_build}" || {
+        error "64-bit dependencies installation failed. Please check the error message and install the missing dependencies manually."
+        exit 1
+      }
+    fi
+    if [[ "$_NOLIB32" != "true" ]]; then
+      install_deps "32" "${_ci_build}" || {
+        error "32-bit dependencies installation failed. Please check the error message and install the missing dependencies manually."
+        exit 1
+      }
+    fi
+  fi
+
+  # this script makes external builds already and we don't want the specific pacman-related stuff to interfere, so enforce _EXTERNAL_INSTALL="false" when not building proton-tkg
+  if [ "$_EXTERNAL_INSTALL" = "true" ]; then
+    _EXTERNAL_INSTALL="false"
+  fi
+
+  # disable faudio check so we don't fail to build even if faudio libs are missing
+  _faudio_ignorecheck="true"
+
+  if [ -z "$_localbuild" ]; then
+    _pkgnaming
+  fi
+
+  # remove the faudio pkgname tag as we can't be sure it'll get used even if enabled
+  pkgname="${pkgname/-faudio-git/}"
+}
+
 nonuser_patcher() {
   if [ "$_NUKR" != "debug" ] || [[ "$_DEBUGANSW1" =~ [yY] ]]; then
     if [ "$_nopatchmsg" != "true" ]; then
@@ -204,8 +233,6 @@ nonuser_patcher() {
 }
 
 build_wine_tkg() {
-  set -e
-
   ## prepare step
   cd "$srcdir"
   # state tracker start - FEAR THE MIGHTY FROG MINER
@@ -214,7 +241,7 @@ build_wine_tkg() {
   if [ "$_SKIPBUILDING" != "true" ]; then
     msg2 "Cloning and preparing sources... Please be patient."
     if [ -z "$_localbuild" ]; then
-      _nomakepkgsrcinit
+      _src_init
       _source_cleanup >>"$_where"/prepare.log
       _prepare
     else
@@ -297,28 +324,76 @@ build_wine_tkg() {
   fi
 }
 
-if [ "$1" = "--deps64" ]; then
-  _nomakepkgsrcinit
-  cd "${srcdir}"/"${_winesrcdir}"
-  ./configure --enable-win64
-  msg2 "You might find help regarding dependencies here: https://github.com/Tk-Glitch/PKGBUILDS/wiki/wine-tkg-git#dependencies"
-elif [ "$1" = "--deps32" ]; then
-  _nomakepkgsrcinit
-  cd "${srcdir}"/"${_winesrcdir}"
-  ./configure
-  msg2 "You might find help regarding dependencies here: https://github.com/Tk-Glitch/PKGBUILDS/wiki/wine-tkg-git#dependencies"
-else
-  # If $1 contains a path, and it exists, use it as default for config
-  if [ -n "$1" ]; then
-    _EXT_CONFIG_PATH="$(readlink -m $1)"
-    if [ ! -f "$_EXT_CONFIG_PATH" ]; then
-      echo "User-supplied external config file '${_EXT_CONFIG_PATH}' not found! Please fix your passed path!"
-      exit 0
-    fi
-    sed -i -e "s|_EXT_CONFIG_PATH.*|_EXT_CONFIG_PATH=${_EXT_CONFIG_PATH}|" "$_where"/wine-tkg-profiles/advanced-customization.cfg
-  fi
+_script_usage() {
+    echo "$0 - A non-makepkg build script for wine-tkg-git"
+    echo ""
+    echo "Usage: $0 [args]"
+    echo ""
+    echo "  Options:"
+    echo ""
+    echo "    -d|--deps64 : Check for missing 64-bit dependencies"
+    echo "    -e|--deps32 : Check for missing 32-bit dependencies"
+    echo "    -c|--config : Use a custom config file"
+    echo ""
+    exit 0
+}
 
-  build_wine_tkg
-fi
+_script_parse_args() {
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -d|--deps64)
+        _DEPSHELPER=1
+        ACTION="deps64"
+        shift 1
+        ;;
+      -e|--deps32)
+        _DEPSHELPER=1
+        ACTION="deps32"
+        shift 1
+        ;;
+      -c|--config)
+        if [ -z "$2" ]; then
+          error "No path provided for custom config file!"
+          exit 1
+        fi
+        _EXT_CONFIG_PATH="$(readlink -m $2)"
+        if [ ! -f "$_EXT_CONFIG_PATH" ]; then
+          echo "User-supplied external config file '${_EXT_CONFIG_PATH}' not found! Please fix your passed path!"
+          exit 0
+        fi
+        sed -i -e "s|_EXT_CONFIG_PATH.*|_EXT_CONFIG_PATH=${_EXT_CONFIG_PATH}|" "$_where"/wine-tkg-profiles/advanced-customization.cfg
+        shift 1
+        ;;
+      *)
+        _script_usage
+        ;;
+    esac
+    shift
+  done
+}
 
+_script_main() {
+  case "$ACTION" in
+    deps64)
+      _src_init
+      cd "${srcdir}"/"${_winesrcdir}"
+      ./configure --enable-win64
+      msg2 "You might find help regarding dependencies here: https://github.com/Tk-Glitch/PKGBUILDS/wiki/wine-tkg-git#dependencies"
+      ;;
+    deps32)
+      _src_init
+      cd "${srcdir}"/"${_winesrcdir}"
+      ./configure
+      msg2 "You might find help regarding dependencies here: https://github.com/Tk-Glitch/PKGBUILDS/wiki/wine-tkg-git#dependencies"
+      ;;
+    build)
+      build_wine_tkg
+      ;;
+  esac
+}
+
+_script_parse_args "$@"
 trap _exit_cleanup EXIT
+_script_init
+_script_main
+
