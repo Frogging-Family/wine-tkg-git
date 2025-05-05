80ff89f81b79d9c6d8e959755c3825436625d657
Tk-Glitch <ti3nou@gmail.com>
2025-05-05 20:59:17 +0200

    Until we get tooling compilation fixed on newer trees, fallback to f5e9c76 on Proton 10 too

--- proton-tkg/proton-tkg.sh
+++ proton-tkg/proton-tkg.sh
@@ -968,7 +968,7 @@ else
     fi
 
     # Tooling compilation needs an update for latest BE - Use slightly older tooling for now
-    if [ -n "$_bleeding_tag" ] || [[ "$_proton_branch" = experimental_8* ]] || [[ "$_proton_branch" = *9* ]]; then
+    if [ -n "$_bleeding_tag" ] || [[ "$_proton_branch" = experimental_8* ]] || [[ "$_proton_branch" = *9* ]] || [[ "$_proton_branch" = *10* ]]; then
       git checkout f5e9c76903e4e18e0416e719a6d42d0cb00998aa
     fi
 
